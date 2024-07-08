f6b50fd1978fb2eb9818df6bcdc37fba3d5b8659
millyanne93 <nmillyanne20@gmail.com>
2024-07-08 15:41:20 +0300

    define login functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, getUser } = require('../controllers/usercontroller');
+const { registerUser, getUser, loginUser } = require('../controllers/usercontroller');
 const auth = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
