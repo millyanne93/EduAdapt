5084b5d376981be2434c0b48088b48e211ad0ec4
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-08 11:58:27 +0300

    Add login and register pages

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/components/AuthForm";
+
+const LoginPage: React.FC = () => {
+    return <AuthForm mode="login" />;
+};
+
+export default LoginPage;
---
+++ frontend/pages/register.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/components/AuthForm";
+
+const RegisterPage: React.FC = () => {
+    return <AuthForm mode="login" />;
+};
+
+export default RegisterPage;
