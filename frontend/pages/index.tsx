e8aec196b3097b179f54526354f80ccc07463be8
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-08 10:35:13 +0300

    Setup frontend project with React and Typescript

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+import React { useEffect, useState } from 'react'
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
