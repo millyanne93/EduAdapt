a763b7b05cf2c978c29907e84e231334361ed365
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-09 15:06:14 +0300

    Modify user credentials on backend

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  class: { type: String, required: true },
+  classname: { type: String, required: true },
   password: { type: String, required: true },
 });
 
