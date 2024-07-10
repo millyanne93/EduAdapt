200720bbd592a885f97b957268dd2d2e1574fe6c
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-10 06:04:24 +0300

    Add questions react component

---
+++ frontend/pages/questions.tsx
@@ -0,0 +1,12 @@
+import QuestionForm from '../components/QuestionForm';
+import Layout from '@/components/Layout';
+
+const QuestionsPage: React.FC = () => {
+  return (
+    <Layout>
+        <QuestionForm />
+    </Layout>
+  );
+};
+
+export default QuestionsPage;
