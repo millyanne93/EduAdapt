4d86425f1d1936a89e6e9bd0fc2dc64f2fe0163d
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-12 10:12:59 +0300

    Add get questions by category method

--- backend/controllers/questioncontroller.js
+++ backend/controllers/questioncontroller.js
@@ -2,24 +2,25 @@ const Question = require('../models/question');
 
 // Create a new question
 exports.createQuestion = async (req, res) => {
-  const { text, difficulty, options, createdBy } = req.body;
+  const { text, options, correctOption, difficulty, topic } = req.body;
 
   try {
-    // Validate the options array
-    if (!options || options.length === 0) {
-      return res.status(400).json({ msg: 'At least one option is required' });
-    }
-
-    // Ensure that there is at least one correct option
-    const hasCorrectOption = options.some(option => option.isCorrect);
-    if (!hasCorrectOption) {
-      return res.status(400).json({ msg: 'At least one option must be marked as correct' });
-    }
-
-    // Create a new question
-    const question = new Question({ text, difficulty, options, createdBy });
+    const question = new Question({ text, options, correctOption, difficulty, topic });
     await question.save();
-    res.status(201).json(question);
+    res.json(question);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+// Get questions by category
+exports.getQuestionsByCategory = async (req, res) => {
+  const { topic } = req.params;
+
+  try {
+    const questions = await Question.find({ topic });
+    res.json(questions);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
@@ -29,8 +30,7 @@ exports.createQuestion = async (req, res) => {
 // Get all questions
 exports.getQuestions = async (req, res) => {
   try {
-    // Find questions and populate the createdBy field
-    const questions = await Question.find().populate('createdBy', 'name email');
+    const questions = await Question.find();
     res.json(questions);
   } catch (err) {
     console.error(err.message);
