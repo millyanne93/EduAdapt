1d013512d49aae223f68e6f14dd3c010450197b8
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-13 20:07:18 +0300

    Add get assessment by id route

--- backend/routes/assessmentRoutes.js
+++ backend/routes/assessmentRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createAssessment, getAssessments } = require('../controllers/assessmentcontroller');
+const { createAssessment, getAssessments, getAssessmentById } = require('../controllers/assessmentcontroller');
 const auth = require('../middleware/authMiddleware');
 
 router.post('/', auth, createAssessment);
 router.get('/', auth, getAssessments);
+router.get('/:id', auth, getAssessmentById);
 
 module.exports = router;
