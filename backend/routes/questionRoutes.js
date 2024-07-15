005a5f6450155faac15682cb8b4e560eebd35b4a
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-15 12:06:10 +0300

    Add get by id, update and delete question routes

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { createQuestion,
-        getQuestionsByCategory,
-        getQuestions } = require('../controllers/questioncontroller');
 const auth = require('../middleware/authMiddleware');
+const {
+    createQuestion,
+    getQuestionsByCategory,
+    getQuestions,
+    getQuestionById,
+    updateQuestion,
+    deleteQuestion
+} = require('../controllers/questioncontroller');
 
 router.post('/', auth, createQuestion);
 router.get('/:topic', auth, getQuestionsByCategory);
 router.get('/', auth, getQuestions);
+router.get('/:id', auth, getQuestionById);
+router.put('/:id', auth, updateQuestion);
+router.delete('/:id', auth, deleteQuestion);
 
 module.exports = router;
