107ae95b5fb8036b9855d33c50877cda4343fe34
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-17 17:22:16 +0300

    Add assessment id in get question response object

--- backend/routes/assessmentRoutes.js
+++ backend/routes/assessmentRoutes.js
@@ -5,14 +5,20 @@ const {
     getAssessments,
     getAssessmentById,
     updateAssessment,
-    deleteAssessment
+    deleteAssessment,
+    getQuestionsForAssessment,
+    createQuestionInAssessment,
+    deleteQuestionFromAssessment
 } = require('../controllers/assessmentcontroller');
 const auth = require('../middleware/authMiddleware');
 
 router.post('/', auth, createAssessment);
+router.post('/:id/questions', auth, createQuestionInAssessment);
 router.get('/', auth, getAssessments);
 router.get('/:id', auth, getAssessmentById);
 router.put('/:id', auth, updateAssessment);
-router.delete('/:id', auth, deleteAssessment)
+router.delete('/:id', auth, deleteAssessment);
+router.get('/:id/questions', auth, getQuestionsForAssessment);
+router.delete('/:assessmentId/questions/:questionId', auth, deleteQuestionFromAssessment)
 
 module.exports = router;
