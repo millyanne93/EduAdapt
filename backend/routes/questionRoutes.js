70a90088556f03d55ff28e778039820f5c817b61
Tony Mputhia <tonymmputhia@gmail.com>
2024-07-17 17:21:27 +0300

    Add create, get and delete methods for questions in assessments

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -11,9 +11,9 @@ const {
 } = require('../controllers/questioncontroller');
 
 router.post('/', auth, createQuestion);
-router.get('/:topic', auth, getQuestionsByCategory);
 router.get('/', auth, getQuestions);
 router.get('/:id', auth, getQuestionById);
+router.get('/category/:topic', auth, getQuestionsByCategory);
 router.put('/:id', auth, updateQuestion);
 router.delete('/:id', auth, deleteQuestion);
 
